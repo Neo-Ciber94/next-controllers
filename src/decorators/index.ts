b5ce2495e23a9ae6f4146580cd67b7a6d66f8b55
Freddy L <neo-ciber_94@hotmail.com>
2021-12-05 13:35:39 -0400

    Updated decorators/index.ts

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -4,6 +4,7 @@ export * from './Delete';
 export * from './Get';
 export * from './Head';
 export * from './OnError';
+export * from './OnNoMatch';
 export * from './Options';
 export * from './Patch';
 export * from './Post';
