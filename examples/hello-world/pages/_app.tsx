bdf3ef27d13914414d22cc0efe5e6d274e498fd8
Freddy L <neo-ciber_94@hotmail.com>
2021-12-19 21:02:58 -0400

    Updated hello-world example

--- examples/hello-world/pages/_app.tsx
+++ examples/hello-world/pages/_app.tsx
@@ -1,11 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 const Layout = ({ children }: { children: React.ReactNode }) => <div className="rainbow-bg">{children}</div>;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <title>Hello World!</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
