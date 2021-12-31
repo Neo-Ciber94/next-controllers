ab2908183df16662e14c81d0d71ba43c27463133
Freddy L <neo-ciber_94@hotmail.com>
2021-12-31 01:49:15 -0400

    Updated UPLOAD_PATH

--- examples/photo-gallery/shared/index.ts
+++ examples/photo-gallery/shared/index.ts
@@ -6,7 +6,7 @@ export const URL_PATH = 'uploads/images/';
 /**
  * Path to store the images.
  */
-export const UPLOAD_PATH = `public/{${URL_PATH}}`;
+export const UPLOAD_PATH = `public/${URL_PATH}`;
 
 /**
  * Name of the field in a multipart/form-data request that contains the image.
