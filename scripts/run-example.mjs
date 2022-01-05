f260f13cacdf5fdf8928ac10083559f136c5198a
Freddy L <neo-ciber_94@hotmail.com>
2022-01-04 20:33:35 -0400

    Changed run-example to mjs to avoid problems executing modules with ts-node

--- scripts/run-example.mjs
+++ scripts/run-example.mjs
@@ -1,8 +1,9 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-console */
 import { argv } from 'process';
 import os from 'os';
 import { spawn } from 'child_process';
-import chalk from 'chalk';
+import chalk from "chalk";
 
 const [projectName] = argv.slice(2);
 
@@ -11,7 +12,7 @@ if (!projectName) {
 }
 
 const exampleDir = `./examples/${projectName}`;
-let cmd: string;
+let cmd;
 
 // https://github.com/nodejs/node/issues/3675#issuecomment-288578092
 if (os.platform() === 'win32') {
@@ -26,6 +27,7 @@ childProccess.once('spawn', () => console.log(`Running example "${projectName}".
 childProccess.on('error', () => {
   const message = chalk.red(`Cannot find the project "${projectName}"`);
   console.error(message);
+  childProccess.kill('SIGINT');
 });
 
 process.on('SIGINT', () => {
